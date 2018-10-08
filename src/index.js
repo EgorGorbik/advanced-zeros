f7a2a0c3e3274a97c84ea8b1882d61d13f06c917
Egor Gorbik <Gorbik1966@mail.ru>
2018-10-08 13:49:21 +0300

    add advanced-zeros task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+	var n = 2;
+	var i = 0;
+	var primeArray=[];
+  var quantityArray=[];
+
+	while (base!=1){
+		if (base % n == 0){
+			base/=n;
+			primeArray[i] = n;
+			if (isNaN(quantityArray[i])){
+				quantityArray[i] = 0;
+			}
+			quantityArray[i]++;	
+		}else {
+			n++;
+			if (primeArray.length==(i+1)){
+				i++;
+			}
+		}
+  } 
+  
+	var sum=[];
+	for (var i = 0; i < primeArray.length; i++){
+		var k = 1;
+		sum[i] = 0;
+		while (k<=number){
+			k*=primeArray[i];
+			sum[i]+=Math.floor(number/k);
+		}
+  }
+  
+	var result = sum[0]/quantityArray[0];
+	for (var i = 0; i<quantityArray.length; i++){
+		if (Math.floor(sum[i]/quantityArray[i])<result){
+			result = Math.floor(sum[i] / quantityArray[i]);
+		}
+	}
+    return result;
 }
